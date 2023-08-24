0fa2ffe9d938b38c993b11fc96187d7166137f6f
Avijit Singh <ajhyont@users.noreply.github.com>
2023-08-23 22:10:16 -0400

    Initial commit; I've created this app as part of testing how to do professional deployments of my code 2 diff cloud-based servers so that I can well, professionally host my custom designed and developed websites. So, This is a basic hello world backend app made for that reason and will b sent 2 the heroku service soon once i do stuff like create a procfile 4 it and include its jar executable file among the project files as well.

---
+++ src/main/java/com/practice/demoDeploymentPractice/DemoDeploymentPracticeApplication.java
@@ -0,0 +1,14 @@
+package com.practice.demoDeploymentPractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDeploymentPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoDeploymentPracticeApplication.class, args);
+		System.out.println("Waheguru ji!");
+	}
+
+}
---
+++ src/test/java/com/practice/demoDeploymentPractice/DemoDeploymentPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.demoDeploymentPractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoDeploymentPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
